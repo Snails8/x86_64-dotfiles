ea2dfb608cf6d44e306cf82928daf7c31e439fda
S-tanishi <tanichi.u.n.20@gmail.com>
2021-11-22 12:57:23 +0900

    シンボリックリンクを自動で貼るシェルの作成

---
+++ install.sh
@@ -0,0 +1,39 @@
+set -u
+
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+git submodule init
+git submodule update
+
+# dotfilesにあるドットファイルをfor文で回し、ホームディレクトリにシンボリックリンクを貼る
+# 正規表現で「.??*」としておけば、先頭がドットで始まり、2文字以上のファイルを探索できる
+# 探索したファイルを「continue」で個別にスキップできる 
+echo "start setup..."
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".require_oh-my-zsh" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    ln -snfv ~/dotfiles/"$f" ~/
+done
+
+[ -e ~/.gitconfig.local ] || cp ~/dotfiles/.gitconfig.local.template ~/.gitconfig.local
+
+# emacs set up
+if which cask >/dev/null 2>&1; then
+  echo "setup .emacs.d..."
+  cd ${THIS_DIR}/.emacs.d
+  cask upgrade
+  cask install
+fi
+
+cat << END
+
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+
+END
