d24c5f92f2118c78d2f89963a807196a3f0d91c0
S-tanishi <tanichi.u.n.20@gmail.com>
2022-01-20 20:57:43 +0900

    add: install 時の挙動変更

--- install.sh
+++ install.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
 set -eu
 
+# ======================================================
 # homebrew がインストールされていない場合インストール(どこで実行してもok )
+# ======================================================
 if [ ! -f /usr/local/bin/brew ]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-  echo "Homebrew has installed."
+  echo "Homebrew has already installed."
 fi
 
-#dotfile ないときはgit clone
-#if [ ! -d ~/dotfiles ]; then
-#  cd ~
-#  git clone git@github.com:comu2e/dotfiles.git
-#else
-#  echo "dotfile is already exist."
-#fi
+# ======================================================
+# dotfile ないときはgit clone
+# ======================================================
+if [ ! -d ~/dotfiles ]; then
+  cd ~
+  git clone git@github.com:Snails8/dotfiles.git
+else
+  echo "dotfile is already exist."
+fi
 
+# ======================================================
+# # set workloads
+# ======================================================
+if [ ! -d ~/works ]; then
+  mkdir ~/works
+fi
+
+if [ ! -d ~/works/private ]; then
+  mkdir ~/works/private
+fi
+
+# ======================================================
 # install software from BrewBundle.
-brew bundle -v --file=~/Brewfile
+# ======================================================
+brew bundle -v --file=~/dotfiles/Brewfile
 
 # if .config has not made ,error will occur.
-#if [ ! -d ~/.config ]; then
-#  mkdir ~/.config
-#fi
+if [ ! -d ~/.config ]; then
+  mkdir ~/.config
+fi
 
+# ======================================================
 # symlink each config file.
-# root の場所を指定した上で実行
 
 # ex) stow -v -d ~/dotfiles/packages/termial/ -t ~ fish
 # ex) ~/dotfiles/packages/termial/fish/.config/fish/*  -> root + .config/fish/*
 # root/.config/fish 配下にシンボリックリンクが作成される (衝突しない)
-
-stow -v -d ~/.myconfig/packages/terminal -t ~ alacritty starship tmux zshrc  gitconfig
-stow -v -d ~/.myconfig/packages/editor -t ~ vimrc vscode
-stow -v -d ~/.myconfig/packages/versioning -t ~ skhd yabai
-#stow -v -d ~/dotfiles/packages/wm -t ~ limelight yabai
-#stow -v -d ~/dotfiles/packages/keybindings -t ~ karabiner
-#ln -sf "~/.config/yabai/yabairc" "~/.yabairc"
-#ln -sf "~/.config/yabai/skhdrc" "~/.skhdrc"
+# ======================================================
+stow -v -d ~/dotfiles/packages/terminal -t ~ alacritty starship tmux zshrc  gitconfig
+stow -v -d ~/dotfiles/packages/editor -t ~ vimrc vscode
+stow -v -d ~/dotfiles/packages/window_tool -t ~ skhd yabai
 
 cat << END
 **************************************************
-DOTFILES SETUP FINISHED! bye.
+DOTFILES SETUP FINISHED! Good Hack!
 **************************************************
 END
