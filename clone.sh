00293487cffbc8bcf3285cd99d1e46df53dd3486
S-tanishi <tanichi.u.n.20@gmail.com>
2022-02-11 20:38:51 +0900

    add: clone 用のコマンド用意

--- clone.sh
+++ clone.sh
@@ -4,33 +4,25 @@ set -eu
 # ======================================================
 # set workloads
 # ======================================================
+if [ ! -f ~/.gitconfig.local ]; then
+  touch ~/.gitconfig.local
+fi
+
+# ======================================================
+# clone
+# ======================================================
 if [ ! -d ~/works ]; then
   mkdir ~/works
 fi
 
 if [ ! -d ~/works/private ]; then
   mkdir ~/works/private
+
+  cd ~/works/private
+  gh repo list Snails8d | awk '{print $1}' |xargs -I {} git clone http://github.com/{}
+  # gh repo list Snails8d | awk '{print $1}' |xargs -I {} cat {}
+
 fi
-
-if [ ! -f ~/.gitconfig.local ]; then
-  touch ~/.gitconfig.local
-fi
-
-
-# イケてないので gh コマンドなどで自動化してやる
-# ======================================================
-# clone
-# ======================================================
-git clone git@github.com:Snails8/laravel-api.git
-git clone git@github.com:Snails8/terraform-module.git
-git clone git@github.com:Snails8/next-spa.git
-
-# ======================================================
-# source は挙動が不安定なので一旦手動で
-# ======================================================
-#source ~/.config/starship.toml # config/.fish に記載してあるので不要
-#source ~/.config/fish/config.fish
-#source ~/.zshrc
 
 cat << END
 **************************************************
